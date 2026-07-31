bdbb8664303ed1c020741543531a2dc31d0fc8d2
Michał Janicki <m.janicki93@gmail.com>
2026-07-31 07:12:57 +0200

    udate chezmoi

--- dot_config/nvim_bak/lua/community.lua
+++ dot_config/nvim_bak/lua/community.lua
@@ -1,10 +1,10 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+--
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
-
----@type LazySpec
+--
+-- -@type LazySpec
 return {
 	"AstroNvim/astrocommunity",
 	{ import = "astrocommunity.pack.lua" },
@@ -16,6 +16,6 @@ return {
 	{ import = "astrocommunity.fuzzy-finder.snacks-picker" },
 	-- { import = "astrocommunity.note-taking.neorg" },
 	{ import = "astrocommunity.editing-support.zen-mode-nvim" },
-	{ import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+	-- { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
 	-- import/override with your plugins folder
 }
